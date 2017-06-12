eb4a4b37a5480c3cd3e033a369fb30e23c641b1d
xiaozhuai <798047000@qq.com>
2017-06-12 17:47:34 +0800

    move version_list to a single js file under devices directory

---
+++ devices/version_list.js
@@ -0,0 +1,14 @@
+var version_list = [
+  {
+    "json": "devices/devices_m.json",
+    "title": "FIUI M"
+  },
+  {
+    "json": "devices/devices_l.json",
+    "title": "FIUI L"
+  },
+  {
+    "json": "devices/devices_kk.json",
+    "title": "FIUI KK"
+  }
+];
